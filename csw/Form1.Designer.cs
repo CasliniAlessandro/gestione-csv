b0e1b69f3e96173a95d6476763f43e84d187e309
caslini <caslini.alessandro.studente@itispaleocapa.it>
2023-05-19 13:59:19 +0200

    aggiunta e contacampi

--- csw/Form1.Designer.cs
+++ csw/Form1.Designer.cs
@@ -30,11 +30,12 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(168, 44);
+            this.textBox1.Location = new System.Drawing.Point(178, 32);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 0;
@@ -47,12 +48,24 @@
             this.listView1.Size = new System.Drawing.Size(316, 423);
             this.listView1.TabIndex = 1;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Aggiunta MIOVALORE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
@@ -66,6 +79,7 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- csw/Form1.cs
+++ csw/Form1.cs
@@ -6,12 +6,85 @@ namespace csw
 {
     public partial class Form1 : Form
     {
+
+
+        public struct punto1
+        {
+            public string r;
+            public string anni;
+            public string stalli;
+            public string Casual;
+        }
+        public int ncampi;
+        public int lunghmax;
+        public punto1[] p;
+
+        public int dim;
+
         public string path;
         public Form1()
         {
             InitializeComponent();
             path = Path.GetFullPath("..\\..\\..\\files\\caslini.csv");
+            dim = 0;
+            p = new punto1[200];
             
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Aggiunta();
+            MessageBox.Show("è stato aggiunto il campo  ");
+        }
+
+        public void Aggiunta(char sep = ';')
+        {
+            string []linea=File.ReadAllLines(path);
+            if (File.Exists(path))
+            {
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    Random r = new Random();
+                    linea[0] += ";miovalore";
+                    sw.WriteLine(linea[0]);
+                    for(int i = 1; i < linea.Length; i++)
+                    {
+                        linea[i] += sep + r.Next(10, 21).ToString();
+                        sw.WriteLine(linea[i]);
+                    }
+                }
+            }
+        }
+
+        public int Contacampi(char sep = ';')
+        {
+            string[]linea=File.ReadAllLines(path);
+            ncampi = linea[0].Split(sep).Length;
+            return ncampi;
+        }
+
+        public int Lunghezza(char sep = ';')
+        {
+            string[] linea = File.ReadAllLines(path);
+
+            for(int i = 0; i < linea.Length; i++)
+            {
+                string[]campi=linea[i].Split(sep);
+                for (int j = 0; j < ncampi; j++)
+                {
+                    if (campi[j].Length > lunghmax)
+                    {
+                        lunghmax = campi[j].Length;
+
+                    }
+                    if (campi[j].Length == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+            return lunghmax;
+        }
     }
+
 }
