59cde8c9c87f155c4b21be1e9e1ff8b31713370a
Alessandro Caslini <caslini.alessandro.studente@itispaleocapa.it>
2023-05-25 15:55:04 +0200

    dalla funzione 4 alla funzione 7

--- csw/Form1.Designer.cs
+++ csw/Form1.Designer.cs
@@ -32,14 +32,27 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 29);
+            this.button1.Location = new System.Drawing.Point(16, 36);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(143, 23);
+            this.button1.Size = new System.Drawing.Size(191, 28);
             this.button1.TabIndex = 2;
             this.button1.Text = " MIOVALORE";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,9 +60,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 70);
+            this.button2.Location = new System.Drawing.Point(16, 86);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(143, 23);
+            this.button2.Size = new System.Drawing.Size(191, 28);
             this.button2.TabIndex = 3;
             this.button2.Text = "CONTACAMPI";
             this.button2.UseVisualStyleBackColor = true;
@@ -57,35 +71,159 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(12, 112);
+            this.button3.Location = new System.Drawing.Point(16, 138);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(143, 23);
+            this.button3.Size = new System.Drawing.Size(191, 28);
             this.button3.TabIndex = 4;
-            this.button3.Text = "button3";
+            this.button3.Text = "LUNGHEZZA MAX RECORD";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(491, 54);
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(273, 36);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 349);
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(434, 430);
             this.dataGridView1.TabIndex = 5;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(16, 188);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(191, 28);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "NUMERO SPAZI";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(16, 242);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(191, 28);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "VISUALIZZA";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(16, 304);
+            this.button6.Margin = new System.Windows.Forms.Padding(4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(191, 28);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "AGGIUNTA CODA";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(742, 101);
+            this.button7.Margin = new System.Windows.Forms.Padding(4);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(280, 28);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "RICERCA";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(742, 54);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(95, 20);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(838, 54);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(95, 20);
+            this.checkBox2.TabIndex = 11;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(939, 54);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(95, 20);
+            this.checkBox3.TabIndex = 12;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(742, 163);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(922, 163);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 14;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -94,6 +232,18 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- csw/Form1.cs
+++ csw/Form1.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace csw
 {
@@ -28,7 +30,7 @@ namespace csw
             path = Path.GetFullPath("..\\..\\..\\files\\caslini.csv");
             dim = 0;
             p = new punto1[200];
-            
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -44,12 +46,25 @@ namespace csw
         {
             MessageBox.Show(LunghezzaMaxRecord().ToString());
         }
-
-
-
+        private void button4_Click(object sender, EventArgs e)
+        {
+            NumeroSpazi();
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Visualizza().ToString());
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(AggiuntaCoda().ToString());
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(ricerca().ToString());
+        }
         public void Aggiunta(char sep = ';')
         {
-            string []linea=File.ReadAllLines(path);
+            string[] linea = File.ReadAllLines(path);
             if (File.Exists(path))
             {
                 using (StreamWriter sw = new StreamWriter(path))
@@ -57,7 +72,7 @@ namespace csw
                     Random r = new Random();
                     linea[0] += ";miovalore";
                     sw.WriteLine(linea[0]);
-                    for(int i = 1; i < linea.Length; i++)
+                    for (int i = 1; i < linea.Length; i++)
                     {
                         linea[i] += sep + r.Next(10, 21).ToString();
                         sw.WriteLine(linea[i]);
@@ -68,7 +83,7 @@ namespace csw
 
         public int Contacampi(char sep = ';')
         {
-            string[]linea=File.ReadAllLines(path);
+            string[] linea = File.ReadAllLines(path);
             ncampi = linea[0].Split(sep).Length;
             return ncampi;
         }
@@ -77,9 +92,9 @@ namespace csw
         {
             string[] linea = File.ReadAllLines(path);
 
-            for(int i = 0; i < linea.Length; i++)
+            for (int i = 0; i < linea.Length; i++)
             {
-                string[]campi=linea[i].Split(sep);
+                string[] campi = linea[i].Split(sep);
                 for (int j = 0; j < ncampi; j++)
                 {
                     if (campi[j].Length > lunghmax)
@@ -98,8 +113,8 @@ namespace csw
 
         public int LunghezzaMaxRecord(char a = ';')
         {
-            string[] record =File.ReadAllLines(path);
-            for(int i = 0; i < record.Length; i++)
+            string[] record = File.ReadAllLines(path);
+            for (int i = 0; i < record.Length; i++)
             {
                 string[] campi = record[i].Split(a);
                 for (int j = 0; j < Contacampi(); j++)
@@ -118,9 +133,92 @@ namespace csw
             return lunghmax;
         }
 
+        public void NumeroSpazi(char a = ';')
+        {
+            string[] record = File.ReadAllLines(path);
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                for (int i = 0; i < record.Length; i++)
+                {
+                    record[i] = record[i].PadRight(200);
+                    sw.WriteLine(record[i]);
+                }
+            }
 
+        }
 
+        public void Visualizza()
+        {
+            dataGridView1.Rows.Clear();
+            string[] record = File.ReadAllLines(path);
+            for (int i = 0; i < record.Length; i++)
+            {
+                string[] campi = record[i].Split(';');
+                if (campi[1] == "")
+                {
+                    dataGridView1.Rows.Add(campi[0]);
+                }
+                else
+                {
+                    dataGridView1.Rows.Add(campi[0], campi[1], campi[2]);
+                }
+            }
+        }
+        public void AggiuntaCoda(string coda1, string coda2, string coda3, char a = ',')
+        {
+            if (File.Exists(path))
+            {
+                using (StreamWriter sw = new StreamWriter(path, true))
+                {
+                    sw.WriteLine(coda1 + a + coda2 + a + coda3 + a);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Non esiste il file");
+            }
+        }
 
+        public bool ricerca()
+        {
+            bool ricerca = false;
+            string[] record = File.ReadAllLines(path);
+            for (int i = 0; i < record.Length; i++)
+            {
+                string[] campi = record[i].Split(';');
+                if (checkBox1.Checked == true)
+                {
+                    if (campi[0].ToLower() == textBox1.Text.ToLower())
+                    {
+                        ricerca = true;
+                        break;
+                    }
+                    if (checkBox2.Checked == true)
+                    {
+                        if (campi[1].ToLower() == textBox2.Text.ToLower())
+                        {
+                            ricerca = true;
+                            break;
+                        }
+                    }
+                    if (checkBox3.Checked == true)
+                    {
+                        if (campi[2].ToLower() == textBox2.Text.ToLower())
+                        {
+                            ricerca = true;
+                            break;
+                        }
+                    }
+                }
+                return ricerca;
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 
 }
+
